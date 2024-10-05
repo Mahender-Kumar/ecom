f5340f8197009b7ec1d397c243512abbfa6eb67f
Mahender Kumar <mksb1947@gmail.com>
2024-10-05 12:16:44 +0530

    auth service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,112 @@
+ 
+import 'package:ecom/utils/show_snackbar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseAuthMethods {
+  final FirebaseAuth _auth;
+  FirebaseAuthMethods(this._auth);
+
+  User get user => _auth.currentUser!;
+
+  // STATE PERSISTENCE STREAM
+  Stream<User?> get authState => FirebaseAuth.instance.authStateChanges();
+
+  // EMAIL SIGN UP
+  Future<void> signUpWithEmail({
+    required String name,
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    try {
+      await _auth
+          .createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      )
+          .then((value) async {
+        // FirebaseAuth.instance.currentUser!.sendEmailVerification();
+        await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+          'email': FirebaseAuth.instance.currentUser!.email,
+          'uid': FirebaseAuth.instance.currentUser!.uid,
+          'name': name,
+        });
+        if (context.mounted) {
+          GoRouter.of(context).go('/');
+        }
+      });
+      // if (context.mounted) {
+      //   await sendEmailVerification(context);
+      // }
+    } on FirebaseAuthException catch (e) {
+      // if you want to display your own custom error message
+      if (e.code == 'weak-password') {
+        if (kDebugMode) {
+          print('The password provided is too weak.');
+        }
+      } else if (e.code == 'email-already-in-use') {
+        if (kDebugMode) {
+          print('The account already exists for that email.');
+        }
+      }
+      if (context.mounted) {
+        showSnackBar(
+            context, e.message!); // Displaying the usual firebase error message
+      }
+    }
+  }
+
+  // EMAIL LOGIN
+  Future<void> loginWithEmail({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      // if (!user.emailVerified) {
+      //   await sendEmailVerification(context);
+      //   // restrict access to certain things using provider
+      //   // transition to another page instead of home screen
+      // }
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!); // Displaying the error message
+    }
+  }
+
+  // EMAIL VERIFICATION
+  Future<void> sendEmailVerification(BuildContext context) async {
+    try {
+      _auth.currentUser!.sendEmailVerification();
+      showSnackBar(context, 'Email verification sent!');
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!); // Display error message
+    }
+  }
+
+  // SIGN OUT
+  Future<void> signOut(BuildContext context) async {
+    try {
+      await _auth.signOut();
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!); // Displaying the error message
+    }
+  }
+
+  // DELETE ACCOUNT
+  Future<void> deleteAccount(BuildContext context) async {
+    try {
+      await _auth.currentUser!.delete();
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!); // Displaying the error message
+ 
+    }
+  }
+}
