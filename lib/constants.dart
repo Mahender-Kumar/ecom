4b69eec2176620c49f8780dd44cc1de399d26045
Mahender Kumar <mksb1947@gmail.com>
2024-10-05 12:16:00 +0530

    colors

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+ import 'package:flutter/material.dart';
+
+ 
+final color1=Color(0xFF0C54BE);
+final color2=Color(0xFF303F60);
+final color3=Color(0xFFF5F9FD);
+final color4=Color(0xFFCED3DC);
+
+
+
+
