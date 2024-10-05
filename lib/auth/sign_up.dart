fcefab5c9abf77fdea05b3a5c343860bf045bfd6
Mahender Kumar <mksb1947@gmail.com>
2024-10-05 12:16:55 +0530

    sign up page

---
+++ lib/auth/sign_up.dart
@@ -0,0 +1,281 @@
+import 'package:ecom/services/auth_service.dart';
+import 'package:ecom/utils/show_snackbar.dart';
+import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  bool isLoading = false;
+
+  void signUpUser() async {
+    context.read<FirebaseAuthMethods>().signUpWithEmail(
+          name: nameController.text,
+          email: emailController.text,
+          password: passwordController.text,
+          context: context,
+        );
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    nameController.dispose();
+    isLoading = false;
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 400,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/background.png'),
+                        fit: BoxFit.fill)),
+                child: Stack(
+                  children: <Widget>[
+                    Positioned(
+                      left: 30,
+                      width: 80,
+                      height: 200,
+                      child: FadeInUp(
+                          duration: const Duration(seconds: 1),
+                          child: Container(
+                            decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage(
+                                        'assets/images/light-1.png'))),
+                          )),
+                    ),
+                    Positioned(
+                      left: 140,
+                      width: 80,
+                      height: 150,
+                      child: FadeInUp(
+                          duration: const Duration(milliseconds: 1200),
+                          child: Container(
+                            decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage(
+                                        'assets/images/light-2.png'))),
+                          )),
+                    ),
+                    Positioned(
+                      right: 40,
+                      top: 40,
+                      width: 80,
+                      height: 150,
+                      child: FadeInUp(
+                          duration: const Duration(milliseconds: 1300),
+                          child: Container(
+                            decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                    image:
+                                        AssetImage('assets/images/clock.png'))),
+                          )),
+                    ),
+                    Positioned(
+                      child: FadeInUp(
+                          duration: const Duration(milliseconds: 1600),
+                          child: Container(
+                            margin: const EdgeInsets.only(top: 50),
+                            child: const Center(
+                              child: Text(
+                                "Sign Up",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          )),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Column(
+                  children: <Widget>[
+                    FadeInUp(
+                        duration: const Duration(milliseconds: 1800),
+                        child: Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                  color:
+                                      const Color.fromRGBO(143, 148, 251, 1)),
+                              boxShadow: const [
+                                BoxShadow(
+                                    color: Color.fromRGBO(143, 148, 251, .2),
+                                    blurRadius: 20.0,
+                                    offset: Offset(0, 10))
+                              ]),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                padding: const EdgeInsets.all(8.0),
+                                decoration: const BoxDecoration(
+                                    border: Border(
+                                        bottom: BorderSide(
+                                            color: Color.fromRGBO(
+                                                143, 148, 251, 1)))),
+                                child: TextFormField(
+                                  controller: nameController,
+                                  decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Name",
+                                      hintStyle:
+                                          TextStyle(color: Colors.grey[700])),
+                                ),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(8.0),
+                                decoration: const BoxDecoration(
+                                    border: Border(
+                                        bottom: BorderSide(
+                                            color: Color.fromRGBO(
+                                                143, 148, 251, 1)))),
+                                child: TextFormField(
+                                  controller: emailController,
+                                  decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Email",
+                                      hintStyle:
+                                          TextStyle(color: Colors.grey[700])),
+                                ),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(8.0),
+                                decoration: const BoxDecoration(
+                                    border: Border(
+                                        bottom: BorderSide(
+                                            color: Color.fromRGBO(
+                                                143, 148, 251, 1)))),
+                                child: TextFormField(
+                                  controller: passwordController,
+                                  decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Password",
+                                      hintStyle:
+                                          TextStyle(color: Colors.grey[700])),
+                                ),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(8.0),
+                                child: TextFormField(
+                                  controller: confirmPasswordController,
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Confirm Password",
+                                      hintStyle:
+                                          TextStyle(color: Colors.grey[700])),
+                                ),
+                              )
+                            ],
+                          ),
+                        )),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    FadeInUp(
+                        duration: const Duration(milliseconds: 1900),
+                        child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              isLoading = true;
+                            });
+                            if (emailController.text.isEmpty ||
+                                !emailController.text.contains('@')) {
+                              showSnackBar(context, 'Invalid email');
+                              // Show an error message
+                            } else if (passwordController.text.isEmpty ||
+                                passwordController.text.length < 6) {
+                              showSnackBar(context,
+                                  'Password must be at least 6 characters');
+
+                              // Show another error message
+                            } else if (passwordController.text !=
+                                confirmPasswordController.text) {
+                              showSnackBar(context, 'Passwords do not match');
+                              // Show another error message
+                            } else {
+                              signUpUser();
+                            }
+                          },
+                          child: Container(
+                            height: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                gradient: const LinearGradient(colors: [
+                                  Color.fromRGBO(143, 148, 251, 1),
+                                  Color.fromRGBO(143, 148, 251, .6),
+                                ])),
+                            child: Center(
+                              child: isLoading
+                                  ? const SizedBox(
+                                      width: 24,
+                                      height: 24,
+                                      child: CircularProgressIndicator(
+                                        strokeCap: StrokeCap.round,
+                                        strokeWidth: 2,
+                                        valueColor:
+                                            AlwaysStoppedAnimation<Color>(
+                                                Colors.white),
+                                      ))
+                                  : const Text(
+                                      "Sign Up",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                            ),
+                          ),
+                        )),
+                    const SizedBox(
+                      height: 48,
+                    ),
+                    FadeInUp(
+                        duration: const Duration(milliseconds: 2000),
+                        child: InkWell(
+                          onTap: () {
+                            context.go('/login');
+                          },
+                          child: const Text(
+                            "Already have an account ? Sign in",
+                            style: TextStyle(
+                                color: Color.fromRGBO(143, 148, 251, 1)),
+                          ),
+                        )),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
