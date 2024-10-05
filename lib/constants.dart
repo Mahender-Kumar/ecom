4445c14615943545e921999e30d472e4faec3a98
Mahender Kumar <mksb1947@gmail.com>
2024-10-05 12:28:01 +0530

    theme data

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,10 @@
  import 'package:flutter/material.dart';
 
  
-final color1=Color(0xFF0C54BE);
-final color2=Color(0xFF303F60);
-final color3=Color(0xFFF5F9FD);
-final color4=Color(0xFFCED3DC);
+const color1=Color(0xFF0C54BE);
+const color2=Color(0xFF303F60);
+const color3=Color(0xFFF5F9FD);
+const color4=Color(0xFFCED3DC);
 
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ecom/app_router.dart';
+import 'package:ecom/constants.dart';
 import 'package:ecom/firebase_options.dart';
 import 'package:ecom/services/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -27,9 +28,76 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       title: 'Dummy Ecom',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        inputDecorationTheme: const InputDecorationTheme(
+          isDense: true,
+          disabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.black54,
+            ),
+          ),
+        ),
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.white),
+        cardTheme: CardTheme(
+          color: Colors.white,
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            side: BorderSide(
+              color: Theme.of(context).colorScheme.outlineVariant,
+            ),
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+        dataTableTheme: DataTableThemeData(
+          dataRowMaxHeight: double.infinity,
+          headingRowColor: WidgetStateProperty.all(
+            Theme.of(context).colorScheme.primaryContainer,
+          ),
+          headingRowHeight: 36,
+          headingTextStyle: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+          dataTextStyle: const TextStyle(
+            fontSize: 12,
+          ),
+          dataRowMinHeight: 36,
+          columnSpacing: 16,
+        ),
+        tabBarTheme: const TabBarTheme(
+          indicatorSize: TabBarIndicatorSize.tab,
+          indicator: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: Color(0xff0161a4),
+                width: 2,
+              ),
+            ),
+            color: Color(0xffd1e4ff),
+          ),
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: color1,
+          brightness: Brightness.light,
+        ),
+        visualDensity: const VisualDensity(vertical: -4, horizontal: -4),
+        scaffoldBackgroundColor: Colors.white,
+        // navigationRailTheme: const NavigationRailThemeData(
+        //   indicatorColor: Color(0xff0161a4),
+        //   selectedIconTheme: IconThemeData(color: Colors.white),
+        // ),
       ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: color1,
+          brightness: Brightness.dark,
+        ),
+        visualDensity: const VisualDensity(vertical: -4, horizontal: -4),
+        scaffoldBackgroundColor: Colors.black,
+        // navigationRailTheme: const NavigationRailThemeData(
+        //   indicatorColor: Color(0xff0161a4),
+        //   selectedIconTheme: IconThemeData(color: Colors.black),
+        // ),
+      ),
+      themeMode: ThemeMode.light,
       debugShowCheckedModeBanner: false,
       routerConfig: AppRouter.instance.router,
     );
