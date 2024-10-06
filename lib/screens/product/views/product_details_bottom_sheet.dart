0dcf21a51194f2a9f0f71c6c2a82b4d5ebdc6387
Mahender Kumar <mksb1947@gmail.com>
2024-10-06 19:23:12 +0530

    product component

---
+++ lib/screens/product/views/product_details_bottom_sheet.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailsBottomSheet extends StatelessWidget {
+  const ProductDetailsBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [],
+      ),
+    );
+  }
+}
