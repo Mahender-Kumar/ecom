301aa8deffc68c7239dd18c89b34757c389cfeba
Mahender Kumar <mksb1947@gmail.com>
2024-10-06 22:34:36 +0530

    deleted

--- lib/constants.dart
+++ lib/constants.dart
@@ -10,12 +10,7 @@ const color4=Color(0xFFCED3DC);
 
  
 // Just for demo
-const productDemoImg1 = "https://i.imgur.com/CGCyp1d.png";
-const productDemoImg2 = "https://i.imgur.com/AkzWQuJ.png";
-const productDemoImg3 = "https://i.imgur.com/J7mGZ12.png";
-const productDemoImg4 = "https://i.imgur.com/q9oF9Yq.png";
-const productDemoImg5 = "https://i.imgur.com/MsppAcx.png";
-const productDemoImg6 = "https://i.imgur.com/JfyZlnO.png";
+ 
 
 // End For demo
 
