e4de4ad3cb485ff357585931b5e080067d360859
Mahender Kumar <mksb1947@gmail.com>
2024-10-06 19:34:25 +0530

    home page

---
+++ lib/screens/home/views/home_screen.dart
@@ -0,0 +1,104 @@
+import 'package:ecom/constants.dart';
+import 'package:ecom/utils/Banner/S/banner_s_style_1.dart';
+import 'package:ecom/utils/Banner/S/banner_s_style_5.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'components/flash_sale.dart';
+import 'components/most_popular.dart';
+import 'components/offer_carousel_and_categories.dart';
+import 'components/popular_products.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        leading: const SizedBox(),
+        leadingWidth: 0,
+        centerTitle: false,
+        title: const Text('Dummy Shop'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Navigator.pushNamed(context, searchScreenRoute);
+            },
+            icon: SvgPicture.asset(
+              "assets/icons/Search.svg",
+              height: 24,
+              colorFilter: ColorFilter.mode(
+                  Theme.of(context).textTheme.bodyLarge!.color!,
+                  BlendMode.srcIn),
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              // Navigator.pushNamed(context, notificationsScreenRoute);
+            },
+            icon: SvgPicture.asset(
+              "assets/icons/Notification.svg",
+              height: 24,
+              colorFilter: ColorFilter.mode(
+                  Theme.of(context).textTheme.bodyLarge!.color!,
+                  BlendMode.srcIn),
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            const SliverToBoxAdapter(child: OffersCarouselAndCategories()),
+            const SliverToBoxAdapter(child: PopularProducts()),
+            const SliverPadding(
+              padding: EdgeInsets.symmetric(vertical: defaultPadding * 1.5),
+              sliver: SliverToBoxAdapter(child: FlashSale()),
+            ),
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  BannerSStyle1(
+                    title: "FLAT",
+                    subtitle: "50% Off",
+                    discountParcent: 50,
+                    press: () {
+                      // Navigator.pushNamed(context, onSaleScreenRoute);
+                    },
+                  ),
+                  const SizedBox(height: defaultPadding / 4),
+                  // We have 4 banner styles, all in the pro version
+                ],
+              ),
+            ),
+            // const SliverToBoxAdapter(child: BestSellers()),
+            const SliverToBoxAdapter(child: MostPopular()),
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  const SizedBox(height: defaultPadding * 1.5),
+
+                  const SizedBox(height: defaultPadding / 4),
+                  // While loading use 👇
+                  // const BannerSSkelton(),
+                  BannerSStyle5(
+                    title: "New \narrival",
+                    subtitle: "SPECIAL OFFER",
+                    bottomText: "Collection".toUpperCase(),
+                    press: () {
+                      // Navigator.pushNamed(context, onSaleScreenRoute);
+                    },
+                  ),
+                  const SizedBox(height: defaultPadding / 4),
+                ],
+              ),
+            ),
+            // const SliverToBoxAdapter(child: BestSellers()),
+          ],
+        ),
+      ),
+    );
+  }
+}
